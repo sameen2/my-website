7cf621c05b2f02d9e360c7d31a018cc683be5da8
Syed Ziaul Ameen <sameen@adobe.com>
2023-04-09 04:15:57 +0530

    minor changes in embed

--- blocks/embed/embed.js
+++ blocks/embed/embed.js
@@ -1,37 +1,3 @@
-// import { loadCSS } from "../../scripts/scripts.js";
-
-const jsonpGist = (url, callback) => {
-	// Setup a unique name that cane be called & destroyed
-	const callbackName = `jsonp_${Math.round(100000 * Math.random())}`;
-
-	// Create the script tag
-	const script = document.createElement("script");
-	script.src = `${url}${
-		url.indexOf("?") >= 0 ? "&" : "?"
-	}callback=${callbackName}`;
-
-	// Define the function that the script will call
-	window[callbackName] = (data) => {
-		delete window[callbackName];
-		document.body.removeChild(script);
-		callback(data);
-	};
-
-	// Append to the document
-	document.body.appendChild(script);
-};
-
-const gist = (element) => {
-	const { href } = element;
-	const url = href.slice(-2) === "js" ? `${href}on` : `${href}.json`;
-
-	jsonpGist(url, (data) => {
-		// loadCSS(data.stylesheet);
-		element.insertAdjacentHTML("afterend", data.div);
-		element.remove();
-	});
-};
-
 const youtube = (element, url) => {
 	const [, vid] = url.split("?v=");
 	const html = `<div class="youtube-wrapper">
@@ -46,8 +12,5 @@ export default function decorate(block) {
 	const url = element.innerText;
 	if (url.includes("youtu")) {
 		youtube(block, url);
-	}
-	if (url.includes("gist")) {
-		gist(block);
 	}
 }
