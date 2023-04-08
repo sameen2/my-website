f1f8a5091b4b31633dd5a2647eb0a38c3d92880b
Syed Ziaul Ameen <sameen@adobe.com>
2023-04-09 04:00:57 +0530

    minor changes in embed

--- blocks/embed/embed.js
+++ blocks/embed/embed.js
@@ -33,13 +33,14 @@ const gist = (element) => {
 };
 
 const youtube = (element) => {
+	console.log(element, block.firstElementChild.children);
 	const url = new URL(element.href);
 	const vid = url.searchParams.get("v");
 	const html = `<div class="youtube-wrapper">
   <iframe src="https://www.youtube.com/embed/${vid}" style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" allowfullscreen="" scrolling="no" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" title="Content from Youtube" loading="lazy"></iframe>
   </div>`;
 	element.innerHTML = "";
-	element.insertAdjacentHTML("afterend", html);
+	element.insertAdjacentHTML("afterbegin", html);
 };
 
 export default function decorate(block) {
