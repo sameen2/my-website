10d1c755d685e98a0bdc44ef9509545785c59c5a
Syed Ziaul Ameen <sameen@adobe.com>
2023-04-13 01:38:24 +0530

    temp commit

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -15,13 +15,16 @@ export default async function decorate(block) {
 			videoPlayToggleBtn.className = "video-play-pause-toggle-button";
 			videoPlayToggleBtn.textContent = "Play";
 			videoPlayToggleBtn.addEventListener("click", (event) => {
-				fetch("/form/sameen2/my-website/main/email-form.json", {
-					method: "POST",
-					headers: {
-						"Content-Type": "application/json",
-					},
-					body: JSON.stringify({ name: "Syed ziaul ameen" }),
-				});
+				fetch(
+					"https://admin.hlx.page/form/sameen2/my-website/main/email-form.json",
+					{
+						method: "POST",
+						headers: {
+							"Content-Type": "application/json",
+						},
+						body: JSON.stringify({ name: "Syed ziaul ameen" }),
+					}
+				);
 				const parent = event.target.closest(".block.video");
 				if (parent) {
 					const video = parent.querySelector("video");
